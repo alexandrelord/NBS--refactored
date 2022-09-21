9b4bb85f3ba0c59402dce1db8b383c51815ed872
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-09-21 08:27:19 -0500

    style create form page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home/Home';
 import Projects from './components/Projects/Index/Projects';
-import ProjectCard from './components/Projects/ProjectCard/ProjectCard';
 import ProjectDetail from './components/Projects/Detail/ProjectDetail';
+import ProjectCreate from './components/Projects/Form/ProjectCreate';
 import NoMatch from './components/NoMatch/NoMatch';
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
             <Route path="/" element={<Layout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="projects" element={<Projects />} />
+                <Route path="projects/create" element={<ProjectCreate />} />
                 <Route path="*" element={<NoMatch />} />
             </Route>
         </Routes>
