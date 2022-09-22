f106a19cb9925a8f6a8b5af5e0b2f942b3f24bb0
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-09-22 18:51:22 -0500

    fix import link for authSlice

--- frontend/src/components/Auth/LoginPage/Login.tsx
+++ frontend/src/components/Auth/LoginPage/Login.tsx
@@ -1,15 +1,15 @@
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { setAuthUser, setIsAuthenticated } from '../../../app/appSlice';
+import { setAuthUser, setIsAuthenticated } from '../../../features/authSlice';
 
 /** CSS Module */
 import styles from './Login.module.css';
 
 /** Google Button */
-import GoogleButton from "react-google-button";
+import GoogleButton from 'react-google-button';
 
 /** Service Functions */
-import {api} from '../../../services/api';
+import { api } from '../../../services/api';
 
 const Login = () => {
     const dispatch = useDispatch();
@@ -20,23 +20,21 @@ const Login = () => {
 
     const fetchUser = async () => {
         try {
-            const response = await api({url: 'http://localhost:4000/users/auth/user', method: 'GET'});
+            const response = await api({ url: 'http://localhost:4000/users/auth/user', method: 'GET' });
             if (response.user) {
                 dispatch(setIsAuthenticated(true));
                 dispatch(setAuthUser(response.user));
                 navigate(prevPath);
             }
-
-            
         } catch (error) {
             console.error(error);
             dispatch(setIsAuthenticated(false));
             dispatch(setAuthUser(null));
             navigate('/login/error');
         }
-    }
-    
-    const handleLogin = async() => {
+    };
+
+    const handleLogin = async () => {
         let timer: NodeJS.Timeout | null = null;
         const googleLoginUrl = 'http://localhost:4000/users/google/login';
         const newWindow = window.open(googleLoginUrl, '_blank', 'width=500,height=600');
@@ -49,7 +47,7 @@ const Login = () => {
                 }
             }, 500);
         }
-    }
+    };
 
     return (
         <div className={styles['buttons-container']}>
