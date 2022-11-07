3b4dcd02f7cc954f79def2be8ecc5e15b27403b7
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-11-07 13:58:06 -0500

    Backend (#11)
    * update lint config file; remove/update dependencies; update config file
    
    * organize folder structure
    
    * remove unused files
    
    * refactor strategies to avoid using cb for mongoose queries
    
    * refactor strategy code

--- backend/src/components/users/strategies/passport-jwt.ts
+++ backend/src/components/users/strategies/passport-jwt.ts
@@ -1,24 +1,25 @@
-import mongoose from 'mongoose';
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import { config } from '../../../config/config';
-import User, { IUser } from '../models/users';
+import User from '../models/users';
 
 const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: config.jwt.accessTokenSecret,
 };
 
-const jwtStrategy = new JwtStrategy(opts, (jwtPayload, done) => {
-    User.findOne({ _id: jwtPayload.sub }, async (err: mongoose.CallbackError, user: IUser) => {
-        if (err) {
-            return done(err, false);
-        }
+const jwtStrategy = new JwtStrategy(opts, async (jwtPayload, done) => {
+    try {
+        const user = await User.findById(jwtPayload.id);
+
         if (user) {
             return done(null, user);
         }
+
         return done(null, false);
-    });
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 export default passport.use(jwtStrategy);
