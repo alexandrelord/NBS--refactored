714989f1be34c7b290e2ecd61ab767d1c7babab2
Alexandre Lord <99230887+alexandrelord@users.noreply.github.com>
2022-11-07 16:06:16 -0500

    add cookieParser to app module

--- backend/src/components/app/app.ts
+++ backend/src/components/app/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 // import apiRules from '../../config/apiRules';
+import cookieParser from 'cookie-parser';
 import logger from '../../middleware/logger';
 import apiRoutes from './routes/api';
 
 const app = express();
 
 // app.use(apiRules);
+app.use(cookieParser());
 app.use(logger);
 app.use(express.json());
 
